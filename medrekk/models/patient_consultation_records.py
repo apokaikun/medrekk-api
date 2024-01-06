5ff4cfd3e7b00f992a580c98e3c0ddb6b911860d
Karl Nonnan Endres <kaiendres@gmail.com>
2024-01-06 10:52:05 +0800

    make other fields optional

--- medrekk/models/patient_consultation_records.py
+++ medrekk/models/patient_consultation_records.py
@@ -1,12 +1,13 @@
+from typing import Optional
 from sqlmodel import Field, String
 from .base import MedRekkBase
 from shortuuid import uuid
 
 
-class MedRekkPatientConsultationRecord(MedRekkBase, table=True):
-    __tablename__ = "medrekk_patient_consultation_records"
+class MedRekkConsultationRecord(MedRekkBase, table=True):
+    __tablename__ = "medrekk_consultation_records"
 
     chiefComplaint: str
-    history: str
-    notes: str
-    management: str
+    history: Optional[str]
+    notes: Optional[str]
+    management: Optional[str]
