621b1aa6852e39ce10d393d16ced38985c188973
Karl Nonnan Endres <kaiendres@gmail.com>
2024-04-05 15:35:47 +0800

    add routes

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI, Query
+from fastapi import Depends, FastAPI
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
 
 from medrekk.controllers.init import init_db
 from medrekk.database.connection import get_db
-from medrekk.routes.auth import auth_routes
-from medrekk.routes.patient_bloodpressures import bloodpressure_routes
-from medrekk.routes.patient_bmi import bmi_routes
-from medrekk.routes.patient_family_history import family_history_routes
-from medrekk.routes.patient_heartrates import heartrate_routes
-from medrekk.routes.patient_height import bodyheight_routes
-from medrekk.routes.patient_hospitalization_history import (
+from medrekk.routes import (
+    allergy_routes,
+    auth_routes,
+    bloodpressure_routes,
+    bmi_routes,
+    bodyheight_routes,
+    bodytemp_routes,
+    bodyweight_routes,
+    family_history_routes,
+    heartrate_routes,
     hospitalization_history_routes,
+    immunization_routes,
+    medical_history_routes,
+    medication_routes,
+    ob_history_routes,
+    patient_routes,
+    record_routes,
+    respiratory_routes,
+    surgical_history_routes,
 )
-from medrekk.routes.patient_medical_history import medical_history_routes
-from medrekk.routes.patient_ob_history import ob_history_routes
-from medrekk.routes.patient_record import record_routes
-from medrekk.routes.patient_respiratory import respiratory_routes
-from medrekk.routes.patient_temperature import bodytemp_routes
-from medrekk.routes.patient_weight import bodyweight_routes
-from medrekk.routes.patients import patient_routes
 
 VERSION = "202403"
 VERSION_SUFFIX = "pre-alpha"
@@ -52,18 +56,24 @@ def root(init: bool, db: Annotated[Session, Depends(get_db)]):
 medrekk_app.include_router(auth_routes)
 
 medrekk_app.include_router(patient_routes)
-# medrekk_app.include_router(bodyweight_routes)
-# medrekk_app.include_router(bodyheight_routes)
-# medrekk_app.include_router(bmi_routes)
+medrekk_app.include_router(bodyweight_routes)
+medrekk_app.include_router(bodyheight_routes)
+medrekk_app.include_router(bmi_routes)
 
 medrekk_app.include_router(family_history_routes)
 medrekk_app.include_router(medical_history_routes)
 medrekk_app.include_router(ob_history_routes)
 
-# medrekk_app.include_router(hospitalization_history_routes)
+medrekk_app.include_router(hospitalization_history_routes)
+medrekk_app.include_router(surgical_history_routes)
+medrekk_app.include_router(medication_routes)
+medrekk_app.include_router(allergy_routes)
+medrekk_app.include_router(immunization_routes)
 
-# medrekk_app.include_router(record_routes)
-# medrekk_app.include_router(bloodpressure_routes)
-# medrekk_app.include_router(heartrate_routes)
-# medrekk_app.include_router(respiratory_routes)
-# medrekk_app.include_router(bodytemp_routes)
+medrekk_app.include_router(record_routes)
+# diagnosis
+# lab results
+medrekk_app.include_router(bloodpressure_routes)
+medrekk_app.include_router(heartrate_routes)
+medrekk_app.include_router(respiratory_routes)
+medrekk_app.include_router(bodytemp_routes)
