fc8eb54f236379910da14afacf4ba0e413cb1071
Karl Nonnan Endres <kaiendres@gmail.com>
2024-04-16 07:36:46 +0000

    Add api key dependency

--- medrekk/common/dependencies/dependencies.py
+++ medrekk/common/dependencies/dependencies.py
@@ -1,5 +1,7 @@
-from fastapi.security import OAuth2PasswordBearer, OAuth2AuthorizationCodeBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2AuthorizationCodeBearer, APIKeyHeader
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth")
 token_scheme = OAuth2AuthorizationCodeBearer(
     authorizationUrl="login", tokenUrl="token")
+
+api_key_scheme = APIKeyHeader(name="X-Api-Key")
