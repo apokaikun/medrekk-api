d2bc783e43b7d39bf4440e36a0fd58b5a65f481d
Karl Nonnan Endres <kaiendres@gmail.com>
2024-05-23 00:41:40 +0000

    change db to db_session

--- medrekk/admin/routes/users.py
+++ medrekk/admin/routes/users.py
@@ -6,7 +6,7 @@ from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
 
 from medrekk.admin.controllers.users import add_account_user, read_user, read_users
-from medrekk.common.database.connection import get_db
+from medrekk.common.database.connection import get_db, get_session
 from medrekk.admin.schemas.accounts import UserCreate, UserListItem, UserRead
 from medrekk.schemas.responses import HTTP_EXCEPTION
 from medrekk.common.utils import routes
@@ -39,13 +39,13 @@ user_routes = APIRouter(
 async def add_user(
     account_id: Annotated[str, Depends(get_account_id)],
     user_data: UserCreate,
-    db: Annotated[Session, Depends(get_db)],
+    db_session: Annotated[Session, Depends(get_session)],
 ):
     """
     Create a new user. Requires `email` and `password`
 
     """
-    user = add_account_user(account_id, user_data, db)
+    user = add_account_user(account_id, user_data, db_session)
     if user is None:
         raise JSONResponse(
             content="HTTP_500_INTERNAL_SERVER_ERROR. The server encountered an unexpected condition that prevented it from fulfilling the request. If the error occurs after several retries, please contact the administrator at: ...",
@@ -72,12 +72,12 @@ Routes that requires a valid jwt_token
 )
 async def get_users(
     account_id: Annotated[str, Depends(get_account_id)],
-    db: Annotated[Session, Depends(get_db)],
+    db_session: Annotated[Session, Depends(get_session)],
 ):
     """
     Get all users.
     """
-    users = read_users(account_id, db)
+    users = read_users(account_id, db_session)
 
     return [UserListItem.model_validate(user) for user in users]
 
@@ -90,9 +90,9 @@ async def get_users(
 async def get_user(
     account_id: Annotated[str, Depends(get_account_id)],
     user_id: str,
-    db: Annotated[Session, Depends(get_db)],
+    db_session: Annotated[Session, Depends(get_session)],
 ):
-    user = read_user(account_id, user_id, db)
+    user = read_user(account_id, user_id, db_session)
 
     return UserRead.model_validate(user)
 
@@ -108,11 +108,11 @@ async def get_user(
 async def delete_user(
     account_id: Annotated[str, Depends(get_account_id)],
     user_id: str,
-    db: Annotated[Session, Depends(get_db)],
+    db_session: Annotated[Session, Depends(get_session)],
 ):
-    user = read_user(account_id, user_id, db)
+    user = read_user(account_id, user_id, db_session)
 
     # Delete the user and return the deleted user
-    db.delete(user)
-    db.commit()
+    db_session.delete(user)
+    db_session.commit()
     return None
