5a103f601722c9d9b9be4cb19e8548a3e627d214
Karl Nonnan Endres <kaiendres@gmail.com>
2024-05-23 01:21:43 +0000

    change db to db_session

--- medrekk/mrs/routes/surgical_history.py
+++ medrekk/mrs/routes/surgical_history.py
@@ -3,21 +3,21 @@ from typing import Annotated, List
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
+from medrekk.common.database.connection import get_session
+from medrekk.common.utils import routes
+from medrekk.common.utils.auth import verify_jwt_token
 from medrekk.mrs.controllers.surgical_history import (
     create_surgical_history,
     delete_surgical_history,
-    read_surgical_history,
     read_surgical_histories,
+    read_surgical_history,
     update_surgical_history,
 )
-from medrekk.common.database.connection import get_db
 from medrekk.mrs.schemas.patients import (
     PatientSurgicalHistoryCreate,
     PatientSurgicalHistoryRead,
     PatientSurgicalHistoryUpdate,
 )
-from medrekk.common.utils import routes
-from medrekk.common.utils.auth import verify_jwt_token
 
 surgical_history_routes = APIRouter(
     prefix=f"/{routes.PATIENTS}" + "/{patient_id}" + f"/{routes.SURGICALHISTORY}",
@@ -34,23 +34,26 @@ surgical_history_routes = APIRouter(
 async def add_patient_surgical_history(
     patient_id: str,
     surgical_history: PatientSurgicalHistoryCreate,
-    db: Annotated[Session, Depends(get_db)],
+    db_session: Annotated[Session, Depends(get_session)],
 ):
-    new_surgical_history = create_surgical_history(patient_id, surgical_history, db)
+    new_surgical_history = create_surgical_history(
+        patient_id, surgical_history, db_session
+    )
 
     return new_surgical_history
+
 
 @surgical_history_routes.get(
     "/",
     response_model=List[PatientSurgicalHistoryRead],
 )
 async def get_patient_surgical_histories(
-    patient_id: str,
-    db: Annotated[Session, Depends(get_db)]
+    patient_id: str, db_session: Annotated[Session, Depends(get_session)]
 ):
-    surgical_histories = read_surgical_histories(patient_id, db)
+    surgical_histories = read_surgical_histories(patient_id, db_session)
 
     return surgical_histories
+
 
 @surgical_history_routes.get(
     "/{surgical_history_id}",
@@ -59,11 +62,14 @@ async def get_patient_surgical_histories(
 async def get_patient_surgical_history(
     patient_id: str,
     surgical_history_id: str,
-    db: Annotated[Session, Depends(get_db)],
+    db_session: Annotated[Session, Depends(get_session)],
 ):
-    surgical_history = read_surgical_history(patient_id, surgical_history_id, db)
+    surgical_history = read_surgical_history(
+        patient_id, surgical_history_id, db_session
+    )
 
     return surgical_history
+
 
 @surgical_history_routes.put(
     "/{surgical_history_id}",
@@ -73,11 +79,14 @@ async def update_patient_surgical_history(
     patient_id: str,
     surgical_history_id: str,
     surgical_history: PatientSurgicalHistoryUpdate,
-    db: Annotated[Session, Depends(get_db)],
+    db_session: Annotated[Session, Depends(get_session)],
 ):
-    updated_surgical_history = update_surgical_history(patient_id, surgical_history_id, surgical_history, db)
+    updated_surgical_history = update_surgical_history(
+        patient_id, surgical_history_id, surgical_history, db_session
+    )
 
     return updated_surgical_history
+
 
 @surgical_history_routes.delete(
     "/{surgical_history_id}",
@@ -86,6 +95,6 @@ async def update_patient_surgical_history(
 async def delete_patient_surgical_history(
     patient_id: str,
     surgical_history_id: str,
-    db: Annotated[Session, Depends(get_db)],
+    db_session: Annotated[Session, Depends(get_session)],
 ):
-    return delete_surgical_history(patient_id, surgical_history_id, db)
+    return delete_surgical_history(patient_id, surgical_history_id, db_session)
