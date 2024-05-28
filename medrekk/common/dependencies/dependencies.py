690b81933e587327d32a8069848af6fea6801989
Karl Nonnan Endres <kaiendres@gmail.com>
2024-05-28 01:04:25 +0000

    cleanup

--- medrekk/common/dependencies/dependencies.py
+++ medrekk/common/dependencies/dependencies.py
@@ -1,7 +1,4 @@
-from fastapi.security import OAuth2PasswordBearer, OAuth2AuthorizationCodeBearer, APIKeyHeader
+from fastapi.security import APIKeyHeader, OAuth2PasswordBearer
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth")
-# token_scheme = OAuth2AuthorizationCodeBearer(
-#     authorizationUrl="login", tokenUrl="token")
-
 api_key_scheme = APIKeyHeader(name="X-Api-Key")
